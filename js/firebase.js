ef55ccd9cae910c6724f06f7e32b04a8db78e076
jason6356 <xiaodemon6536@gmail.com>
2023-03-31 22:20:27 +0800

    Adding the tutorial on how to use git :)

--- js/firebase.js
+++ js/firebase.js
@@ -3,3 +3,15 @@ console.log('Hello world');
 console.log(' NO more hello world  :)')
 
 //ghjlgjjh
+
+
+
+// Pull - Synchorize Change
+
+// Edit Code : )
+
+// Stage File - Add file to update
+
+// Add commit message
+
+// Commit
