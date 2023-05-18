823e13623bee58c22bb5bb7c29727edab9038a6f
Rexalez <129522029+Rexalez@users.noreply.github.com>
2023-05-19 00:23:02 +0800

    Updated  working project details

--- js/project.js
+++ js/project.js
@@ -29,7 +29,7 @@ database.ref('Project').orderByKey().once('value')
       const projectDetail = document.createElement('div');
       projectDetail.classList.add('col-12');
       projectDetail.innerHTML = `
-      <div class="card m-2" id="${projectSnapshot.key}">
+      <div class="card m-2" data-id = "${projectSnapshot.key}">
         <div class="row align-items-center">
           <div class="image-parent col-12 col-lg-4">
             <img src="${project.imageUrls[0]}" class="img-fluid" alt="" style="height: 200px;width: 100%;">
@@ -40,7 +40,7 @@ database.ref('Project').orderByKey().once('value')
                 ${project.companyName}
               </h5>
               <div class="d-block d-lg-none col-2 text-end">
-                <a href="/html/projectDetails.html" class="stretched-link"><i class="arrow fa-solid fa-angle-right"></i></a>
+                <a href="#" class="stretched-link"><i class="arrow fa-solid fa-angle-right"></i></a>
               </div>
             </div>
             <div class="row justify-content-center">
@@ -50,7 +50,7 @@ database.ref('Project').orderByKey().once('value')
                 </p>
               </div>
               <div class="d-none d-lg-block col-lg-1 ">
-                <a href="/html/projectDetails.html" class="stretched-link"><i class="arrow fa-solid fa-angle-right"></i></a>
+                <a href="#" class="stretched-link"><i class="arrow fa-solid fa-angle-right"></i></a>
               </div>
             </div>
           </div>
@@ -65,29 +65,10 @@ database.ref('Project').orderByKey().once('value')
     let projectButton = document.querySelectorAll(".stretched-link");
     projectButton.forEach(pojectBtn => {
       pojectBtn.addEventListener("click", () => {
-        let projectId = pojectBtn.parentElement.parentElement.parentElement.parentElement.dataset.id;
+        let projectId = pojectBtn.parentElement.parentElement.parentElement.parentElement.parentElement.dataset.id;
 
-        window.location.assign("/html/projectDetails.html");
+        window.location.href = 'projectDetails.html?projectId=' + projectId;
 
-        projectRef.child(projectId).get().then((snapshot => {
-          //console.log(snapshot.val());
-
-          let companyName = snapshot.val().companyName;
-          let companyCaption = snapshot.val().companyCaption;
-          let generation = snapshot.val().generation;
-          let treesPlanted = snapshot.val().treesPlanted;
-          let CO2Offset = snapshot.val().CO2Offset;
-          let description = snapshot.val().description;
-          let imgUrls = snapshot.val().snapshot.val().imageUrls;
-
-          document.getElementById("companyName").innerHTML = companyName;
-          document.getElementById("companyCaption").innerHTML = companyCaption;
-          document.getElementById("generation").innerHTML = generation;
-          document.getElementById("tressPlanted").innerHTML = treesPlanted;
-          document.getElementById("CO2Offset").innerHTML = CO2Offset;
-          document.getElementById("description").innerHTML = description;
-
-        }))
       });
     });
 
