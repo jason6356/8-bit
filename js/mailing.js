3d2c8172cf045d0a2b57cb29d165abc3fa5113f9
Rexalez <129522029+Rexalez@users.noreply.github.com>
2023-05-19 03:05:45 +0800

    Added Subscription functionality

--- js/mailing.js
+++ js/mailing.js
@@ -2,3 +2,34 @@
 // 1 - User input email and send.
 // 2 - Firebase will store the email into email list and send a confirmation
 // 3 - Company can send any subsciption/news/contents thorugh these email
+
+let subscribeButton = document.querySelector("#subscribe form");
+
+subscribeButton.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    console.log('Subscribe Button Clicked');
+
+    let email = document.getElementById("subscribeEmail").value;
+
+    // Get a reference to the "subscription" node
+    let subscriptionRef = database.ref('Subscription');
+    
+    // Generate a new child node ID using push()
+    let newSubscriptionRef = subscriptionRef.push();
+    
+    // Get the generated subscription ID
+    let subscriptionId = newSubscriptionRef.key;
+    
+    // Set the subscription data using the generated ID
+    newSubscriptionRef.set({
+      subscriptionEmail: email,
+    }).then(function() {
+      alert("Subscribed!");
+      console.log("Subscribed!");
+      location.reload();
+    }).catch(function(error) {
+      console.log("Error Subscribing:", error);
+    });
+    
+})
