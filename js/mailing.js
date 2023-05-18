a63d48eb521ab5ab26c4fc16cb0697760fa01dd7
junsheng526 <junshengchoo57@gmail.com>
2023-05-18 20:19:28 +0800

    update mailing (not yet implement)

---
+++ js/mailing.js
@@ -0,0 +1,4 @@
+// footer mailing
+// 1 - User input email and send.
+// 2 - Firebase will store the email into email list and send a confirmation
+// 3 - Company can send any subsciption/news/contents thorugh these email
