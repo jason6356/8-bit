8b6654be96e8972f2594be59375a1e3d7790cb3a
junsheng526 <junshengchoo57@gmail.com>
2023-05-19 01:15:09 +0800

    update loading animation

--- js/projectDetails.js
+++ js/projectDetails.js
@@ -8,21 +8,28 @@ const firebaseConfig = {
     appId: "1:724597515303:web:6bddc00b7e17d7d3ee1a02",
     measurementId: "G-KY5QTKQZPD"
 };
-  
+
 firebase.initializeApp(firebaseConfig);
 
 const projectRef = firebase.database().ref('Project');
 
 const urlParams = new URLSearchParams(window.location.search);
 const projectId = urlParams.get('projectId');
+const projectImage = document.getElementById('projectImage[0]');
+const loadingSpinner = document.querySelector('.loadingSpinner');
 
 console.log(projectId);
 
 projectRef.child(projectId).get().then((snapshot => {
+    loadingSpinner.style.display = 'block';
 
     document.getElementById("companyName").textContent = snapshot.val().companyName;
     document.getElementById("companyCaption").textContent = snapshot.val().companyCaption;
-    document.getElementById("projectImage[0]").src = snapshot.val().imageUrls[0];
+    projectImage.onload = () => {
+        // Hide the loading spinner
+        loadingSpinner.style.display = 'none';
+    };
+    projectImage.src = snapshot.val().imageUrls[0];
     document.getElementById("generation").setAttribute("data-val", snapshot.val().generation);
     document.getElementById("treesPlanted").setAttribute("data-val", snapshot.val().treesPlanted);
     document.getElementById("CO2Offset").setAttribute("data-val", snapshot.val().CO2offset);
@@ -31,7 +38,7 @@ projectRef.child(projectId).get().then((snapshot => {
     projectRef.child(projectId).get().then((snapshot) => {
         const imageUrls = snapshot.val().imageUrls;
         const imageUrlsContainer = document.getElementById("imageUrls");
-    
+
         imageUrls.forEach((imageUrl) => {
             const swiperSlide = document.createElement("div");
             swiperSlide.className = "swiper-slide img-fluid";
