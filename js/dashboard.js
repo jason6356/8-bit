d4176ecf16ae3fe866c941b3989293f322a12304
junsheng526 <junshengchoo57@gmail.com>
2023-05-21 20:12:33 +0800

    update

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,37 +1,3 @@
-// var xValues = ["Jan", "Feb", "Mar", "Apr", "May"];
-// var yValues = [55, 49, 44, 24, 15];
-// var barColors = "#AEC2B6";
-// // chart colors
-// var colors = ['#007bff', '#28a745', '#333333', '#c3e6cb', '#dc3545', '#6c757d'];
-
-// new Chart("bar", {
-//     type: "bar",
-//     data: {
-//         labels: xValues,
-//         datasets: [{
-//             data: [589, 445, 483, 503, 689, 692, 634],
-//             backgroundColor: colors[0]
-//         },
-//         {
-//             data: [639, 465, 493, 478, 589, 632, 674],
-//             backgroundColor: colors[1]
-//         }]
-//     },
-//     options: {
-//         legend: { display: false },
-//         title: {
-//             display: true,
-//             text: "Sunergy Bar Chart"
-//         },
-//         scales: {
-//             xAxes: [{
-//                 barPercentage: 0.4,
-//                 categoryPercentage: 0.5
-//             }]
-//         }
-//     }
-// });
-
 // Function to update the bar chart
 function updateBarChart(serviceCounts) {
     var barData = Object.values(serviceCounts);
