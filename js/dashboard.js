ec0d4c6970a70abda9b11a42e062102730bf6924
junsheng526 <junshengchoo57@gmail.com>
2023-05-21 19:35:01 +0800

    update bar chart

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,36 +1,68 @@
-var xValues = ["Jan", "Feb", "Mar", "Apr", "May"];
-var yValues = [55, 49, 44, 24, 15];
-var barColors = "#AEC2B6";
-// chart colors
-var colors = ['#007bff', '#28a745', '#333333', '#c3e6cb', '#dc3545', '#6c757d'];
+// var xValues = ["Jan", "Feb", "Mar", "Apr", "May"];
+// var yValues = [55, 49, 44, 24, 15];
+// var barColors = "#AEC2B6";
+// // chart colors
+// var colors = ['#007bff', '#28a745', '#333333', '#c3e6cb', '#dc3545', '#6c757d'];
 
-new Chart("bar", {
-    type: "bar",
-    data: {
-        labels: xValues,
-        datasets: [{
-            data: [589, 445, 483, 503, 689, 692, 634],
-            backgroundColor: colors[0]
-        },
-        {
-            data: [639, 465, 493, 478, 589, 632, 674],
-            backgroundColor: colors[1]
-        }]
-    },
-    options: {
-        legend: { display: false },
-        title: {
-            display: true,
-            text: "Sunergy Bar Chart"
-        },
-        scales: {
-            xAxes: [{
-                barPercentage: 0.4,
-                categoryPercentage: 0.5
+// new Chart("bar", {
+//     type: "bar",
+//     data: {
+//         labels: xValues,
+//         datasets: [{
+//             data: [589, 445, 483, 503, 689, 692, 634],
+//             backgroundColor: colors[0]
+//         },
+//         {
+//             data: [639, 465, 493, 478, 589, 632, 674],
+//             backgroundColor: colors[1]
+//         }]
+//     },
+//     options: {
+//         legend: { display: false },
+//         title: {
+//             display: true,
+//             text: "Sunergy Bar Chart"
+//         },
+//         scales: {
+//             xAxes: [{
+//                 barPercentage: 0.4,
+//                 categoryPercentage: 0.5
+//             }]
+//         }
+//     }
+// });
+
+// Function to update the bar chart
+function updateBarChart(serviceCounts) {
+    var barData = Object.values(serviceCounts);
+
+    var ctx = document.getElementById('barChart');
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: Object.keys(serviceCounts),
+            datasets: [{
+                label: 'Service Types',
+                data: barData,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.7)',
+                    'rgba(54, 162, 235, 0.7)',
+                    'rgba(255, 206, 86, 0.7)',
+                    'rgba(75, 192, 192, 0.7)',
+                    'rgba(153, 102, 255, 0.7)'
+                ],
             }]
+        },
+        options: {
+            title: {
+                display: true,
+                text: "Inbox Service Types Bar Chart"
+            },
+            responsive: true,
+            maintainAspectRatio: false
         }
-    }
-});
+    });
+}
 
 
 //-------------------------------------------------------------------------------------------------------------------------------------
@@ -57,6 +89,7 @@ function countServiceTypes() {
             }
         });
         console.log(serviceCounts);
+        updateBarChart(serviceCounts);
         updateDoughnutChart(serviceCounts);
     });
 }
